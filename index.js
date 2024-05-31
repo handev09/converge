30405fcc98f20545ddfd686d82594ba71fa65a4f
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2024-05-31 03:47:46 +0300

    changes

--- index.js
+++ index.js
@@ -1,39 +1,85 @@
-const express = require('express');
-const axios = require('axios');
-const querystring = require('querystring');
+const express = require("express");
+const axios = require("axios");
+const querystring = require("querystring");
 
 const app = express();
 const port = 3000;
 
 async function checkoutRequest(req, res) {
-  console.log(req.query)
-  const { ssl_merchant_id, ssl_user_id, ssl_pin, ssl_transaction_type, ssl_amount } = req.query;
+  console.log(req.query);
+  const {
+    ssl_merchant_id,
+    ssl_user_id,
+    ssl_pin,
+    ssl_transaction_type,
+    ssl_amount,
+    ssl_first_name,
+    ssl_last_name,
+    ssl_email,
+    ssl_phone,
+    ssl_avs_address,
+    ssl_avs_zip,
+    ssl_city,
+    ssl_state,
+    ssl_country,
+    ssl_ship_to_address1,
+    ssl_ship_to_city,
+    ssl_ship_to_country,
+    ssl_ship_to_first_name,
+    ssl_ship_to_last_name,
+    ssl_ship_to_phone,
+    ssl_ship_to_state,
+    ssl_ship_to_zip,
+    ssl_merchant_txn_id,
+  } = req.query;
 
   const requestBody = querystring.stringify({
     ssl_merchant_id,
     ssl_user_id,
     ssl_pin,
     ssl_transaction_type,
-    ssl_amount
+    ssl_amount,
+    ssl_first_name,
+    ssl_last_name,
+    ssl_email,
+    ssl_phone,
+    ssl_avs_address,
+    ssl_avs_zip,
+    ssl_city,
+    ssl_state,
+    ssl_country,
+    ssl_ship_to_address1,
+    ssl_ship_to_city,
+    ssl_ship_to_country,
+    ssl_ship_to_first_name,
+    ssl_ship_to_last_name,
+    ssl_ship_to_phone,
+    ssl_ship_to_state,
+    ssl_ship_to_zip,
+    ssl_merchant_txn_id,
   });
 
   const options = {
     headers: {
-      'referer': 'https://www.terragroupranges.com',
-      'content-type': 'application/x-www-form-urlencoded'
-    }
+      referer: "https://www.terragroupranges.com",
+      "content-type": "application/x-www-form-urlencoded",
+    },
   };
 
   try {
-    const response = await axios.post('https://api.convergepay.com/hosted-payments/transaction_token', requestBody, options);
+    const response = await axios.post(
+      "https://api.convergepay.com/hosted-payments/transaction_token",
+      requestBody,
+      options
+    );
     res.json(response.data);
   } catch (err) {
-    console.log('The request to the Checkout API failed.', err);
-    res.status(500).json({ error: 'The request to the Checkout API failed.' });
+    console.log("The request to the Checkout API failed.", err);
+    res.status(500).json({ error: "The request to the Checkout API failed." });
   }
 }
 
-app.get('/checkout', checkoutRequest);
+app.get("/checkout", checkoutRequest);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
